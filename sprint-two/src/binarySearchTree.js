e39d5b37cefd9624fddb6f5186ab9cc32a1f977b
aliSnakesou <lkollasfbt@gmail.com>
2020-03-18 19:56:48 +0100

    implementation of contain methods for BST

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -6,36 +6,66 @@ class Node {
   }
 }
 
-class BinarySearchTree {
-  constructor(){
-      this.root = null ;
+
+  class BinarySearchTree {
+    constructor(){
+        this.root = null ;
+    }
+
+  addChild(data) {
+
+    var node = this.root;
+    //  if the root is null , e will add the first node wich is the parent or the root
+    if(node === null){
+        this.root = new Node(data);
+    }else{
+        var check = function(node){
+            if(data < node.data){
+                if(node.left === null){
+                    node.left = new Node(data)
+                }else if(node.left !==null){
+                    return check(node.left)
+                }
+            }else if(data > node.data){
+                if(node.right === null){
+                    node.right = new Node(data)
+                }else if(node.right !== null){
+                    return check(node.right)
+                }
+            }else{
+                // the value is equal to our node value
+                return null ;
+            }
+        };
+        return check(node)
+    }
   }
+  containChild(data) {
+    var current = this.root ;
+    while(current){
+        if(data === current.data) {
+            return true ;
+        }
+        // the search will be easier because our tree is
+        // kind of sorted so everytime we will need just to compare the input with the current data
+        // than we will decide in what part we will search on
+        if(data < current.data){
+            current = current.left
+        }else{
+            current = current.right;
+        }
+    }
 
-addChild(data) {
-
-  var node = this.root;
-  //  if the root is null , e will add the first node wich is the parent or the root
-  if(node === null){
-      this.root = new Node(data);
-  }else{
-      var check = function(node){
-          if(data < node.data){
-              if(node.left === null){
-                  node.left = new Node(data)
-              }else if(node.left !==null){
-                  return check(node.left)
-              }
-          }else if(data > node.data){
-              if(node.right === null){
-                  node.right = new Node(data)
-              }else if(node.right !== null){
-                  return check(node.right)
-              }
-          }
-      }
+    return false;
   }
 }
-}
+
+
+
+
+
+
+
 
 
 // var BinarySearchTree = function(value) {
