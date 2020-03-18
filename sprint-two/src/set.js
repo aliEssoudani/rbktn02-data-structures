ad167b272ff9a6f292e103e52fd2a39e8dee01fb
aliSnakesou <lkollasfbt@gmail.com>
2020-03-18 18:38:06 +0100

    set finished

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,34 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  var x = this._storage ;
+  if(x.indexOf(item)=== -1){
+    x.push(item)
+  }else{
+     return "this item already exist"}
 };
 
 setPrototype.contains = function(item) {
+  var x = this._storage ;
+ if(x.indexOf(item) !== -1){
+   return true
+ }else{
+   return false
+ }
 };
 
 setPrototype.remove = function(item) {
+  var index = this._storage.indexOf(item) ;
+  if(index !== -1){
+    this._storage.splice(index, 1)
+  }
+
 };
 
 /*
