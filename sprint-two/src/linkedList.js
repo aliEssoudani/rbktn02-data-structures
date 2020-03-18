e8a30484a19537520ff20d5a37d40ac3108f711b
aliSnakesou <lkollasfbt@gmail.com>
2020-03-18 16:10:40 +0100

    linked list at home

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -62,3 +62,87 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+class Node{
+  constructor(data, next = null){
+      this.data = data;
+      this.next = next;
+  }
+}
+
+class LinkedList{
+  constructor(){
+      this.head = null;
+      this.size = 0;
+  }
+  // add node at first
+  addatFirst(data){
+   this.head = new Node(data, this.head);
+   this.size++
+  }
+  // add node at last
+  addatLast(data){
+   var node = new Node(data);
+   let current;
+   if(!this.head){
+      this.head = node;
+   }else{
+      current = this.head;
+      while(current.next){
+       current = current.next
+      }
+      current.next = node;
+
+
+
+   }
+   this.size++;
+
+  }
+  // insert node at index
+  insertAtInd(data, index){
+      if(index > 0 && index > this.size){
+          return
+      }
+      if(index === 0){
+       this.head = new Node(data, this.head)
+       return
+
+      }
+      const node  = new Node(data);
+      let current,previous;
+      current = this.head;
+      let count = 0;
+      while(count < index){
+          previous = current;
+          current = current.next;
+          count++;
+
+      }
+      node.next = current;
+      previous.next = node
+      this.size++
+  }
+  // getat(inde)
+  getat(index){
+   let current = this.head;
+   let count = 0;
+
+   while(current){
+       if(count == index){
+           console.log(current.data)
+
+       }
+       count++;
+       current = current.next
+   }
+   return ;
+  }
+  // print data
+  prinList(){
+      let current = this.head;
+      while(current){
+          console.log(current.data);
+          current = current.next;
+      }
+  }
+}
